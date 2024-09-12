f6a05904fd7157a81e74f34db95511977b04aca0
Naufil Shaikh <snaufil09@gmail.com>
2024-09-12 08:10:55 +0530

    Added docker orch support

--- src/Services/Catalog/Catalog.Api/Program.cs
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -2,7 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to container.
 builder.Services.AddCarter();
-builder.Services.AddMediatR(config => { config.RegisterServicesFromAssembly(typeof(Program).Assembly); });
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly); 
+});
 builder.Services.AddMarten(options =>
 {
     options.Connection(builder.Configuration.GetConnectionString("MicroservicesDB")!);
