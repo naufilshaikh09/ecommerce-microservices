7a518f5ced698c65919247239a9df13ac328c76c
Naufil Shaikh <snaufil09@gmail.com>
2024-09-22 09:31:43 +0530

    Added containerization support for order api and for migration on order infrastructure.

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure.Data.Extensions;
+
+public static class DatabaseExtensions
+{
+    public static async Task InitializeDatabaseAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        context.Database.MigrateAsync().GetAwaiter().GetResult();
+    }
+}
