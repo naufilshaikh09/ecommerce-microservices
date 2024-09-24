d6ab39f975a37874fb8d846374fa36422d75c798
Naufil Shaikh <snaufil09@gmail.com>
2024-09-24 08:08:25 +0530

    Implemented command and query for the order.appliation project with the help of mediatr and cqrs pattern

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -0,0 +1,11 @@
+namespace Ordering.Application.Orders.EventHandlers.Domain;
+
+public class OrderCreatedEventHandler
+    (ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
+{
+    public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+    }
+}
